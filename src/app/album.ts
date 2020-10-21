bb932233c1d60a5971d4e3432c0377d0956c3149
Tomasz Lukaszyk <tomasz.lukaszyk@avanade.com>
2020-10-21 13:51:01 +0200

    Fix the album interface

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,8 +1,12 @@
 import { Track } from './track';
 
 export interface Album {
-  name: string
-  releaseDate: string
-  coverImage: string
-  tracks: Track[]
+  id: number;
+  artist: string;
+  album: {
+    name: string
+    releaseDate: string
+    coverImage: string
+    tracks: Track[]
+  }
 }
